a7161978a1298405465e784ad3fc1c1f8598e3b2
Zack Hatlen <zckhtln@icloud.com>
2023-03-24 15:38:49 +0300

    master

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
-<template>
-  <div>
-    {{ product }}
-  </div>
-</template>
-
 <script setup>
 const { data } = await useAsyncGql('getProduct', { 
   slug: 'patterned-wrap-skirt'
 });
 const product = data.value.product;
 </script>
+
+<template>
+  <div>
+    {{ product }}
+  </div>
+</template>
