36f0f6a7199838566d9b929f919d057213ef641a
Zack Hatlen <zckhtln@icloud.com>
2023-03-28 10:09:41 +0300

    Update graphqlNuxtClient.vue

--- components/graphqlNuxtClient.vue
+++ components/graphqlNuxtClient.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    {{ reviews }}
+    <div>Avarege: {{ reviews.averageRating }}</div>
+    <br>
+    <div v-for="review in reviews.edges" :key="review.id">
+      <div>
+        <div style="position: relative;display: flex;align-items: center;">
+          <img style="width: 30px;border-radius: 999px" v-if="review.node.author.node.avatar" :src="review.node.author.node.avatar.url" />
+          <span style="font-size: 18px;font-weight: 600;margin-left: 19px">{{ review.node.author.node.name }}</span>
+          <span style="color: #FFC107;margin-left: 19px;font-size: 18px;font-weight: 600;">{{ review.rating }}</span>
+          <span style="font-size: 18px;font-weight: 600;margin-left: 19px">{{ review.node.date }}</span>
+        </div>
+      </div>
+      <div style="font-size: 20px;" v-html="review.node.content"></div>
+    </div>
   </div>
 </template>
 
